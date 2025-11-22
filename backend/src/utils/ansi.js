1292efc51a55021697afdcccca8b16ee92ac54b8
mulln <153391808+mulln@users.noreply.github.com>
2025-11-21 23:06:17 -0500

    Update ansi.js

--- backend/src/utils/ansi.js
+++ backend/src/utils/ansi.js
@@ -1,1 +1,10 @@
+export const ansi = {
+  bold: (t) => `\x1b[1m${t}\x1b[0m`,
+  red: (t) => `\x1b[31m${t}\x1b[0m`,
+  green: (t) => `\x1b[32m${t}\x1b[0m`,
+  yellow: (t) => `\x1b[33m${t}\x1b[0m`,
+  blue: (t) => `\x1b[34m${t}\x1b[0m`,
+  magenta: (t) => `\x1b[35m${t}\x1b[0m`,
+  cyan: (t) => `\x1b[36m${t}\x1b[0m`,
+};
 
