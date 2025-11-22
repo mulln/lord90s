c72f6b0225efa330c4897052ada56a2c72b62d71
mulln <153391808+mulln@users.noreply.github.com>
2025-11-21 23:05:49 -0500

    Update database.js

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,1 +1,20 @@
+import Database from "better-sqlite3";
+import fs from "fs";
+import path from "path";
+
+import config from "../config.js";
+
+const dbFile = config.dbPath;
+const schemaPath = path.join(process.cwd(), "backend/src/db/schema.sql");
+
+const dbExists = fs.existsSync(dbFile);
+const db = new Database(dbFile);
+
+if (!dbExists) {
+  const schema = fs.readFileSync(schemaPath, "utf8");
+  db.exec(schema);
+  console.log("SQLite database created & schema applied.");
+}
+
+export default db;
 
