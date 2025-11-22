438b02058fb5c5f2dfc0444f4252bedcb66f0127
mulln <153391808+mulln@users.noreply.github.com>
2025-11-21 23:12:59 -0500

    Update terminal.js

--- frontend/src/terminal.js
+++ frontend/src/terminal.js
@@ -1,1 +1,36 @@
+let term;
+
+export function initTerminal() {
+  term = new Terminal({
+    cursorBlink: true,
+    allowTransparency: true,
+    theme: {
+      background: "#000000",
+      foreground: "#31d7ff",
+      cursor: "#31d7ff"
+    }
+  });
+
+  term.open(document.getElementById("terminal"));
+  resizeTerminal();
+
+  window.addEventListener("resize", resizeTerminal);
+
+  return term;
+}
+
+function resizeTerminal() {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  document.getElementById("terminal").style.width = `${w}px`;
+  document.getElementById("terminal").style.height = `${h}px`;
+}
+
+export function write(text) {
+  term.write(text.replace(/\n/g, "\r\n"));
+}
+
+export function clear() {
+  term.clear();
+}
 
